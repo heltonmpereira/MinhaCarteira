1656538d5cc7343aebf87c2ca354477cbf2f9a43
Helton M. Pereira <heltonmpereira@hotmail.com>
2026-06-30 14:17:00 -0300

    Correções

--- src/MinhaCarteira.Servico/Helper/Criptografia.cs
+++ src/MinhaCarteira.Servico/Helper/Criptografia.cs
@@ -1,49 +1,27 @@
-﻿using System;
-using System.Linq;
-using System.Security.Cryptography;
+using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace MinhaCarteira.Servico.Helper;
 
 public static class Criptografia
 {
+    private static readonly PasswordHasher<object> _passwordHasher = new();
+
     public static string CriptografarTexto(this string texto)
     {
-        byte[] salt;
-        byte[] buffer2;
-        if (texto == null)
-            throw new ArgumentNullException(nameof(texto));
-
-        using (Rfc2898DeriveBytes bytes = new(texto, 0x10, 0x3e8, HashAlgorithmName.SHA1))
-        {
-            salt = bytes.Salt;
-            buffer2 = bytes.GetBytes(0x20);
-        }
-        byte[] dst = new byte[0x31];
-        Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
-        Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
-        return Convert.ToBase64String(dst);
+        return texto == null 
+            ? throw new ArgumentNullException(nameof(texto)) 
+            : _passwordHasher.HashPassword(new object(), texto);
     }
 
     public static bool VerificarHashSenha(this string senhaCifrada, string senha)
     {
-        byte[] buffer4;
+        ArgumentNullException.ThrowIfNull(senha);
+
         if (senhaCifrada == null)
             return false;
 
-        if (senha == null)
-            throw new ArgumentNullException(nameof(senha));
-
-        byte[] src = Convert.FromBase64String(senhaCifrada);
-        if (src.Length != 0x31 || src[0] != 0)
-            return false;
-
-        byte[] dst = new byte[0x10];
-        Buffer.BlockCopy(src, 1, dst, 0, 0x10);
-        byte[] buffer3 = new byte[0x20];
-        Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
-        using (Rfc2898DeriveBytes bytes = new(senha, dst, 0x3e8, HashAlgorithmName.SHA1))
-            buffer4 = bytes.GetBytes(0x20);
-
-        return buffer3.SequenceEqual(buffer4);
+        var result = _passwordHasher.VerifyHashedPassword(new object(), senhaCifrada, senha);
+        return result == PasswordVerificationResult.Success || result == PasswordVerificationResult.SuccessRehashNeeded;
     }
 }
